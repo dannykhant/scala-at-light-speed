38876bb6610f77d32869c91b6f32e561a47d68be
danny <dannypmkhant@gmail.com>
2025-06-27 23:21:36 +0700

    problem solved: find the difference

--- src/main/scala/com/saanay/Problem49.scala
+++ src/main/scala/com/saanay/Problem49.scala
@@ -1,14 +1,27 @@
 package com.saanay
 
 object Problem49 {
+
   // merge strings alternately
-  object Solution {
-    def mergeAlternately(word1: String, word2: String): String = {
-      val result = new StringBuilder
-      for ((x, y) <- word1.zipAll(word2, "", "")) {
-        result ++= s"$x$y"
-      }
-      result.toString
+  def mergeAlternately(word1: String, word2: String): String = {
+    val result = new StringBuilder
+    for ((x, y) <- word1.zipAll(word2, "", "")) {
+      result ++= s"$x$y"
     }
+    result.toString
   }
+
+  // find the difference
+  def findTheDifference(s: String, t: String): Char = {
+    val ss = s.sorted
+    val ts = t.sorted
+    val pairs = ss.zipAll(ts, "", "")
+    val cs = pairs.collectFirst { case (x, y) if x != y => y }.get
+    val c: Char = cs match {
+      case ch: Char => ch
+      case str: String => str.head
+    }
+    c
+  }
+
 }
